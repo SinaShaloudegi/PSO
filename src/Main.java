64dee8e527f45bdedf1a956d9513e4d3cddccc1e
sina <sinashaloudegi@gmail.com>
2017-07-10 16:18:11 +0430

    Initilize2

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,22 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         initilize();
     }
 
     private static void initilize() {
         Swarm swarm = new Swarm();
+
         swarm.init();
         for (int i = 0; i < swarm.particles.length; i++) {
             System.out.println(" x : " + swarm.particles[i].x
                     + " lb : " + swarm.particles[i].lb
                     + " v : " + swarm.particles[i].v
-                    + " fitness : " + swarm.particles[i].fitness
-                    + " gb : " + swarm.gb);
+                    + " fitness : " + swarm.particles[i].fitness);
 
         }
+        System.out.println("Global Best : " + swarm.gb);
     }
 
 
